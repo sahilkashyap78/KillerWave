dfc34e16c6f69e25b577771d945dd803bc959b4e
DESKTOP-MMSUSHT\admin <sahilkashyap78@gmail.com>
2021-12-05 10:04:27 -0800

    Initial Scripts setup

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		LightSetup();
+		CameraSetup();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void CameraSetup()
+    {
+		Debug.Log("Camera Setup	done");
+		GameObject mainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+		//Camera Transform
+		mainCamera.transform.position = new Vector3(0f, 0f, -300f);
+		mainCamera.transform.eulerAngles = new Vector3(0f, 0f, 0f);
+
+		//camera background
+
+		mainCamera.GetComponent<Camera>().clearFlags = CameraClearFlags.SolidColor;
+		mainCamera.GetComponent<Camera>().backgroundColor = new Color32(0, 0, 0, 255);
+
+	}
+
+	private void LightSetup()
+    {
+		GameObject dirLight = GameObject.Find("Directional Light");
+		dirLight.transform.eulerAngles = new Vector3(50, -30, 0);
+		dirLight.GetComponent<Light>().color =
+		new Color32(152, 204, 255, 255);
+	}
+}
---
+++ Assets/Resources/Scripts/IActorTemplate.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IActorTemplate {
+	int sendDamage();
+	void TakeDamage(int incomingDamage);
+	void Die();
+	void ActorStates();
+}
