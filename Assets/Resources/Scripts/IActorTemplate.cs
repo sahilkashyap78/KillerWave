63d6086df84165f6a39b9b6a775b6663d27a9f2c
DESKTOP-MMSUSHT\admin <sahilkashyap78@gmail.com>
2021-12-07 10:11:35 -0800

    Player Bullet setup

--- Assets/Resources/Scripts/IActorTemplate.cs
+++ Assets/Resources/Scripts/IActorTemplate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public interface IActorTemplate {
-	int sendDamage();
+	int SendDamage();
 	void TakeDamage(int incomingDamage);
 	void Die();
 	void ActorStates();
---
+++ Assets/Resources/Scripts/PlayerBullet.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour {
+
+	GameObject actor;
+	int hitPower;
+	int health;
+	int travelSpeed;
+	[SerializeField]
+	SOActorModel bulletModel;
+
+	void Awake()
+	{
+		ActorStats(bulletModel);
+	}
+
+	void Update()
+	{
+		transform.position += new
+		Vector3(travelSpeed, 0, 0) * Time.deltaTime;
+	}
+
+
+	public int SendDamage()
+	{
+		return hitPower;
+	}
+	public void TakeDamage(int incomingDamage)
+	{
+		health -= incomingDamage;
+	}
+
+	public void Die()
+	{
+		Destroy(this.gameObject);
+	}
+
+	void OnBecameInvisible()
+	{
+		Destroy(gameObject);
+	}
+
+	public void ActorStats(SOActorModel actorModel)
+	{
+		hitPower = actorModel.hitPower;
+		health = actorModel.health;
+		travelSpeed = actorModel.speed;
+		actor = actorModel.actor;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Enemy")
+		{
+			if (other.GetComponent<IActorTemplate>() != null)
+			{
+				if (health >= 1)
+				{
+					health -= other.GetComponent<IActorTemplate>
+					().SendDamage();
+				}
+				if (health <= 0)
+				{
+					Die();
+				}
+			}
+		}
+	}
+
+
+	// Update is called once per frame
+	
+}
