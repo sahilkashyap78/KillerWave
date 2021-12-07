30717d496a50630e1877370baae0d285f1a85f31
DESKTOP-MMSUSHT\admin <sahilkashyap78@gmail.com>
2021-12-07 09:52:09 -0800

    Scriptable Objects Setup

---
+++ Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    int travelSpeed;
+    int health;
+    int hitPower;
+    GameObject actor;
+    GameObject fire;
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+    public GameObject Fire
+    {
+        get { return fire; }
+        set { fire = value; }
+    }
+    GameObject _Player;
+    float width;
+    float height;
+    // Use this for initialization
+    void Start()
+    {
+        //here height and width gets converted to viewpot space from world space
+        height = 1 / (Camera.main.WorldToViewportPoint(new
+        Vector3(1, 1, 0)).y - .5f);
+        width = 1 / (Camera.main.WorldToViewportPoint(new Vector3(1, 1, 0))
+        .x - .5f);
+        _Player = GameObject.Find("_Player");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Attack();
+    }
+
+    private void Movement()
+    {
+
+    }
+
+    private void Attack()
+    {
+
+    }
+
+    public void ActorStats(SOActorModel actorModel)
+    {
+        health = actorModel.health;
+        travelSpeed = actorModel.speed;
+        hitPower = actorModel.hitPower;
+        fire = actorModel.actorsBullets;
+    }
+
+}
---
+++ Assets/Resources/Scripts/PlayerSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	SOActorModel actorModel;
+	GameObject playerShip;
+	// Use this for initialization
+	void Start () {
+		CreatePlayer();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreatePlayer()
+	{
+		//CREATE PLAYER
+		actorModel = Object.Instantiate(Resources.Load
+		("Scripts/ScriptableObjects/Player_Default"))
+		as SOActorModel;
+		playerShip = GameObject.Instantiate(actorModel.actor)
+		as GameObject;
+		playerShip.GetComponent<Player>().ActorStats(actorModel);
+		//SET PLAYER UP
+		playerShip.transform.rotation = Quaternion.Euler(0, 180, 0);
+		playerShip.transform.localScale = new Vector3(60, 60, 60);
+		playerShip.name = "Player";
+		playerShip.transform.SetParent(this.transform);
+		playerShip.transform.position = Vector3.zero;
+	}
+}
+
