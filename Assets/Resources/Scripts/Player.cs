dfeb61276374b3e6ce78a5204ca84ad9b5af7f0b
DESKTOP-MMSUSHT\admin <sahilkashyap78@gmail.com>
2021-12-07 10:03:26 -0800

    Player scrip setup done

--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -43,12 +43,58 @@ public class Player : MonoBehaviour
 
     private void Movement()
     {
-
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            if (transform.localPosition.x < width + width / 0.9f)
+            {
+                transform.localPosition += new Vector3
+                (Input.GetAxisRaw("Horizontal")
+                * Time.deltaTime * travelSpeed, 0, 0);
+            }
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            if (transform.localPosition.x > width + width / 6)
+            {
+                transform.localPosition += new Vector3
+                (Input.GetAxisRaw("Horizontal")
+                * Time.deltaTime * travelSpeed, 0, 0);
+            }
+        }
+        if (Input.GetAxisRaw("Vertical") < 0)
+        {
+            if (transform.localPosition.y > -height / 3f)
+            {
+                transform.localPosition += new Vector3
+                (0, Input.GetAxisRaw("Vertical") * Time.deltaTime * travelSpeed, 0);
+            }
+        }
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (transform.localPosition.y < height / 2.5f)
+            {
+                transform.localPosition += new Vector3
+                (0, Input.GetAxisRaw("Vertical") * Time.deltaTime * travelSpeed, 0);
+            }
+        }
     }
+
+    public void Die()
+    {
+        Destroy(this.gameObject);
+    }
+
 
     private void Attack()
     {
-
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bullet = GameObject.Instantiate
+            (fire, transform.position, Quaternion.Euler
+            (new Vector3(0, 0, 0))) as GameObject;
+            bullet.transform.SetParent(_Player.transform);
+            bullet.transform.localScale = new Vector3(7, 7, 7);
+        }
     }
 
     public void ActorStats(SOActorModel actorModel)
@@ -57,6 +103,40 @@ public class Player : MonoBehaviour
         travelSpeed = actorModel.speed;
         hitPower = actorModel.hitPower;
         fire = actorModel.actorsBullets;
+    }
+
+
+    public void TakeDamage(int incomingDamage)
+    {
+        health -= incomingDamage;
+    }
+    public int SendDamage()
+    {
+        return hitPower;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            if (health >= 1)
+            {
+                if (transform.Find("energy +1(Clone)"))
+                {
+                    Destroy(transform.Find("energy +1(Clone)").gameObject);
+                    health -= other.GetComponent<IActorTemplate>
+                    ().SendDamage();
+                }
+                else
+                {
+                    health -= 1;
+                }
+            }
+            if (health <= 0)
+            {
+                Die();
+            }
+        }
     }
 
 }
